2aea086a2d9c21dcec732da956c6a7653abccc68
Karolina25 <karolinagonzales25@gmail.com>
2022-01-21 19:50:55 -0500

    last componentAcommodation

--- src/app/Pages/Home/HomePage.js
+++ src/app/Pages/Home/HomePage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import HomeTemplate from "../../Templates/HomeTemplate/HomeTemplate";
-import CardHighly from "../../Componentes/CardHighly/CardHighly";
 
 const HomePage = ()=>{
     return(
