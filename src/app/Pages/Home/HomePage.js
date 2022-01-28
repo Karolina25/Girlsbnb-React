e87179f0aea2f737c4c8b09300516019fa0093cc
Karolina25 <karolinagonzales25@gmail.com>
2022-01-27 22:37:04 -0500

    implemented apiService and make template

--- src/app/Pages/Home/HomePage.js
+++ src/app/Pages/Home/HomePage.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from "react";
 import HomeTemplate from "../../Templates/HomeTemplate/HomeTemplate";
 import {getAll} from "../../../data/api/ApiService";
+import CardGroupOfSix from "../../Componentes/CardGroupOfSix/CardGroupOfSix";
+import Featured from "../../Componentes/Featured/Featured";
 
 const HomePage = ()=>{
   const [accommodation, setAccommodation]=useState([]);
@@ -16,8 +18,21 @@ const HomePage = ()=>{
     getAccommodation();
   }, []);
   
-    return(
+    return(<>
           <HomeTemplate accommodation={accommodation} />
+          <CardGroupOfSix
+          description="Multi-day hackatons organized by local experts with activities, meals and accommodation included"
+          title="Discover Chairdnb adventures"
+          />
+          <CardGroupOfSix
+          description="Multi-day extreme programming sessions organized by local experts with activities, meals and accommodation included"
+          title="Highly rated experiences"
+          />
+          <Featured
+            verifiedDescription="More than 200 verified stays"
+            description="From 577 € / person - 3 days"
+          />
+          </>
     )
 }
 
